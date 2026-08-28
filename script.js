a489956c78266ceefa76e4a756c386026a662c4e
Lukasalksater <lukalk0428@gmail.com>
2026-08-28 12:00:06 +0200

    updated html, css and js file to reflect the new look

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-// Sätter aktuellt år i footern automatiskt
+// Sets the current year in the footer automatically
 document.getElementById("year").textContent = new Date().getFullYear();
 
-// Enkel ljust/mörkt tema-växlare
+// Simple light/dark theme toggle
 const toggleBtn = document.getElementById("theme-toggle");
 
 toggleBtn.addEventListener("click", () => {
   document.body.classList.toggle("light");
 
   if (document.body.classList.contains("light")) {
-    toggleBtn.textContent = "Byt tema ☀️";
+    toggleBtn.textContent = "Toggle theme ☀️";
   } else {
-    toggleBtn.textContent = "Byt tema 🌙";
+    toggleBtn.textContent = "Toggle theme 🌙";
   }
 });
